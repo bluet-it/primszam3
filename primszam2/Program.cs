3bd0534d05715ce61581a6d5df3cd3b7e036523a
bluet-it <vince@turcsanyivince.hu>
2020-02-13 18:44:07 +0100

    Add files via upload

---
+++ primszam2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primszam2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    int n = int.Parse(Console.ReadLine());
+                    bool k = true;
+                    for (int j = 2; j <= Math.Sqrt(n); j++)
+                    {
+                        if (n % j == 0)
+                        {
+                            k = false;
+                            Console.Write(j.ToString() + "|");
+                        }
+                    }
+                    if (k)
+                    {
+                        Console.WriteLine("igen");
+                    }
+                    else {
+                        Console.Write(n.ToString() + "|");
+                        Console.WriteLine("nem");
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("hiba");
+                }
+
+            }
+        }
+    }
+}
